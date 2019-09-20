1880c4ade79d19f8c7172fddc1d0d7f46905a550
princio <l.principi8@gmail.com>
2019-09-20 13:23:29 +0000

    Update darknet_to_movidius.sh

--- darknet_to_movidius.sh
+++ darknet_to_movidius.sh
@@ -1,9 +1,43 @@
 #! /bin/bash
 
-# required .cfg .weights .names
+echo "Execution must be done from PIMOVITY folder!!!"
 
-flow --model yolov2.cfg --load yolov2.weights  --savepb
+# required .cfg .weights .names
+NAME="$1"
+__PATH="./data/yolov2/ale/$NAME/"
+FILE="${__PATH}$NAME.zip"
+
+echo "File is: «$FILE»."
+
+if [ ! -f $FILE ]; then
+    echo "Zip file not exists! Exit."
+    exit 1
+fi
+
+cd $__PATH
+
+RET=`unzip $NAME.zip`
+if [[ $RET -gt 0 ]]; then
+    echo "Unzip filed."
+    exit 1
+fi
+
+
+if [ ! -f $NAME.weights ]; then
+    echo "$NAME.weights file not exists! Exit."
+    exit 1
+fi
+if [ ! -f $NAME.cfg ]; then
+    echo "$NAME.cfg file not exists! Exit."
+    exit 1
+fi
+if [ ! -f labels.txt ]; then
+    echo "Labels.txt file not exists! Exit."
+    exit 1
+fi
+
+flow --model $NAME.cfg --load $NAME.weights  --savepb
 
 export PYTHONPATH="${PYTHONPATH}:/opt/movidius/caffe/python"
 
-mvNCCompile yolov2.pb -s 12 -in input -on output -o yolov2.graph
+mvNCCompile $NAME.pb -s 12 -in input -on output -o $NAME.graph
