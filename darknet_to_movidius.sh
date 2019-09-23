f07142183160b828c93302930bf76e6c778576a7
princio <l.principi8@gmail.com>
2019-09-23 15:55:45 +0200

    Update darknet_to_movidius.sh

--- darknet_to_movidius.sh
+++ darknet_to_movidius.sh
@@ -2,10 +2,14 @@
 
 echo "Execution must be done from PIMOVITY folder!!!"
 
+if [ $# -eq 0 ]; then
+    echo "Too less arguments."
+    exit 1
+fi
 # required .cfg .weights .names
-NAME="$1"
-__PATH="./data/yolov2/ale/$NAME/"
-FILE="${__PATH}$NAME.zip"
+FILE="$1"
+__PATH="./data/yolov2/ale/"
+NAME="${FILE:0:-4}"
 
 echo "File is: «$FILE»."
 
@@ -14,14 +18,19 @@ if [ ! -f $FILE ]; then
     exit 1
 fi
 
-cd $__PATH
-
-RET=`unzip $NAME.zip`
+RET=`mkdir -p $NAME`
 if [[ $RET -gt 0 ]]; then
-    echo "Unzip filed."
+    echo "mkdir failed."
     exit 1
 fi
 
+RET=`unzip $NAME.zip -d $__PATH/$NAME`
+if [[ $RET -gt 0 ]]; then
+    echo "Unzip failed."
+    exit 1
+fi
+
+cd $__PATH/$NAME
 
 if [ ! -f $NAME.weights ]; then
     echo "$NAME.weights file not exists! Exit."
@@ -38,11 +47,13 @@ fi
 
 flow --model $NAME.cfg --load $NAME.weights  --savepb
 
-cd built_graph
-
 export PYTHONPATH="${PYTHONPATH}:/opt/movidius/caffe/python"
 
 mvNCCompile $NAME.pb -s 12 -in input -on output -o $NAME.graph
 
-mv $NAME.graph ../../yolov2-tiny-$NAME.graph
-mv $NAME.meta ../../yolov2-tiny-$NAME.meta
+mv $NAME.meta ../../yolov2-tiny-ale-$NAME.meta
+mv $NAME.graph ../../yolov2-tiny-ale-$NAME.graph
+
+cd ../..
+
+rm -r $NAME
